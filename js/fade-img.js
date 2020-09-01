9bca04e07350287396d244f44585478c0e711d65
Carlgro <guerreroaguilar182@gmail.com>
2020-09-01 12:28:54 -0500

    Correccion Imagenes

--- js/fade-img.js
+++
@@ -1,7 +0,0 @@
-console.log(12)
-
-
-let bkimg = 'https://images.unsplash.com/photo-1598130340340-3b590382f2df?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60';
-
-
-document.getElementById("img2").style.backgroundImage = url('./img/bote.png');
